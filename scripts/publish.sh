02cbff28c1fe461a4dc8311d6cd975ecc3a422a0
Shlomi Assaf (shlassaf) <shlassaf@cisco.com>
2017-07-21 02:18:35 +0300

    misc: commit version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
-#npm publish ./dist_package/@tdm/core
-#npm publish ./dist_package/@tdm/data
-#npm publish ./dist_package/@tdm/ngx-dynamic-forms
-#npm publish ./dist_package/@tdm/ngx-http
-#npm publish ./dist_package/@tdm/json-api-mapper
-#npm publish ./dist_package/@tdm/tixin
+npm publish ./dist_package/@tdm/core
+npm publish ./dist_package/@tdm/data
+npm publish ./dist_package/@tdm/ngx-dynamic-forms
+npm publish ./dist_package/@tdm/ngx-http
+npm publish ./dist_package/@tdm/json-api-mapper
+npm publish ./dist_package/@tdm/tixin
