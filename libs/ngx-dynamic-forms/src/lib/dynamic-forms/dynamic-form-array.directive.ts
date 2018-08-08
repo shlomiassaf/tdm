c134f3b5ac4c5f837c5d788a28aae8aa848c138d
Shlomi Assaf <shlomiasaf@gmail.com>
2018-08-09 00:09:34 +0300

    fix previous fix, support angular 6.0 as well as 6.1

--- libs/ngx-dynamic-forms/src/lib/dynamic-forms/dynamic-form-array.directive.ts
+++ libs/ngx-dynamic-forms/src/lib/dynamic-forms/dynamic-form-array.directive.ts
@@ -114,7 +114,21 @@ export class ForFormArrayDirective extends NgForOf<DynamicControlRenderContext>
       if (changes.fArray) {
         const controls = this.fArray && this.fArray.controls;
         this.trySetDiffer(controls);
-        this.ngForOf = this.ngForOfRef = [];
+
+        // Backward compat for angular < 6.1
+        // https://github.com/angular/angular/commit/08a18b82de003c379aa76323e65e8230e5b42a56#diff-e63254bca5a65cfdc11f078011bddf98
+        // TODO: remove when not supporting 6.0.0
+        if (super.ngOnChanges) {
+          changes.ngForOf = new SimpleChange(
+            this.ngForOf,
+            [],
+            changes.fArray.isFirstChange()
+          );
+          this.ngForOf = this.ngForOfRef = changes.ngForOf.currentValue;
+          super.ngOnChanges(changes);
+        } else {
+          this.ngForOf = this.ngForOfRef = [];
+        }
       }
     }
   }
